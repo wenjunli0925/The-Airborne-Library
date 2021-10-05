721c2d8869d6476fe78450a3b2830e35a0ad3aab
Wenjun Li <liw820@newschool.edu>
2021-10-04 23:47:24 -0400

    Improve UI Interaction

--- Assets/DataFiles/Scripts/Rotation.cs
+++ Assets/DataFiles/Scripts/Rotation.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class Rotation : MonoBehaviour
 {
+    public int xdirection;
+    public int ydirection;
     public int zdirection;
+
+    private int xRotation;
+    private int yRotation;
+    private int zRotation;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        xRotation = Random.Range(0, xdirection);
+        yRotation = Random.Range(0, ydirection);
+        zRotation = Random.Range(0, zdirection);
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        transform.Rotate(new Vector3(0, 0, zdirection) * Time.deltaTime);
+        transform.Rotate(new Vector3(xRotation, yRotation, zRotation) * Time.deltaTime);
     }
 }
