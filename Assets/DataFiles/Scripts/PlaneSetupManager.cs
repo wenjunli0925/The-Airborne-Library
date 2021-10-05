1a8a4b64c381a9140d91df4f9fa0d655fdd1354f
Wenjun Li <liw820@newschool.edu>
2021-10-04 22:22:13 -0400

    Add TextShpere_Test Scene

---
+++ Assets/DataFiles/Scripts/PlaneSetupManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class PlaneSetupManager : MonoBehaviour
+{
+    public ARPlaneManager planeManager;
+    public Material occlusionMat, planeMat;
+    public GameObject planePrefab;
+
+    public void SetOcclusionMaterail()
+    {
+        planePrefab.GetComponent<Renderer>().material = occlusionMat;
+
+        foreach (var plane in planeManager.trackables)
+        {
+            plane.GetComponent<Renderer>().material = occlusionMat;
+        }
+    }
+
+    public void SetPlanMaterail()
+    {
+        planePrefab.GetComponent<Renderer>().material = planeMat;
+
+        foreach (var plane in planeManager.trackables)
+        {
+            plane.GetComponent<Renderer>().material = planeMat;
+        }
+    }
+
+}
